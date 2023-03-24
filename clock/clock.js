535f4bcb8930310a61218cc3d101f0406a69fd03
ProdigiousMelon <thelag@gmail.com>
2023-03-24 10:02:14 -0700

    rollback multiple clocks js

--- clock/clock.js
+++ clock/clock.js
@@ -9,18 +9,7 @@ function currentTime() {
     min = updateTime(min);
     sec = updateTime(sec);
 
-    /*For my time*/
-    var dLA=d.toLocaleString("en-US",{timeZone:"America/LOS_ANGELES"});
-    var mhour = dLA.getHours();
-    var mmin = dLA.getMinutes();
-    var msec = dLA.getSeconds();
-    mhour = updateTime(mhour);
-    mmin = updateTime(mmin);
-    msec = updateTime(msec);
-
     document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
-    document.getElementById("my_clock").innerText = mhour + " : " + mmin + " : " + msec; /* adding time to the div */
-
     var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
   }
   
