1efe8b32692f8a5832e1e65fe388f954fc9af2b1
ProdigiousMelon <thelag@gmail.com>
2023-03-24 09:33:24 -0700

    Added local clock

--- clock/clock.js
+++ clock/clock.js
@@ -1,5 +1,6 @@
 /* create a digital clock and put it in a box in javascript*/
 function currentTime() {
+    /*For the your time*/ 
     var date = new Date(); /* creating object of Date class */
     var hour = date.getHours();
     var min = date.getMinutes();
@@ -7,7 +8,19 @@ function currentTime() {
     hour = updateTime(hour);
     min = updateTime(min);
     sec = updateTime(sec);
+
+    /*For my time*/
+    var dLA=d.toLocaleString("en-US",{timeZone:"America/LOS_ANGELES"});
+    var mhour = dLA.getHours();
+    var mmin = dLA.getMinutes();
+    var msec = dLA.getSeconds();
+    mhour = updateTime(mhour);
+    mmin = updateTime(mmin);
+    msec = updateTime(msec);
+
     document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
+    document.getElementById("my_clock").innerText = mhour + " : " + mmin + " : " + msec; /* adding time to the div */
+
     var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
   }
   
