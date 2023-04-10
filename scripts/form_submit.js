296b92c00b9462faf33e9d0893d6566da591d9b8
ProdigiousMelon <thelag@gmail.com>
2023-04-10 13:50:57 -0700

    formatting changes for formsub

--- scripts/form_submit.js
+++ scripts/form_submit.js
@@ -1,20 +1,22 @@
-const form = document.querySelector("#form")
-   const submitButton = document.querySelector("#submit")
-   const scriptURL = 'https://script.google.com/macros/s/AKfycbzmZxjCm7RaRyaENzPZ6rwADNv9C9JtDRg5Cs9tb9zdbOjOYvq1Vl-EXp8dPfkHgIt2iA/exec'
+function process(){
+   const form = document.querySelector("#form");
+   const submitButton = document.querySelector("#submit");
+   const scriptURL = 'https://script.google.com/macros/s/AKfycbz3Ujf76JQLG00mjTKqdO9rKHK3TxCiKEDldeawMg70fzZw1aB5VMnElXtHp5R0lNxlSw/exec';
 
    form.addEventListener('submit', e => {
-     submitButton.disabled = true
-     e.preventDefault()
-     let requestBody = new FormData(form)
+     submitButton.disabled = true;
+     e.preventDefault();
+     let requestBody = new FormData(form);
      fetch(scriptURL, { method: 'POST', body: requestBody})
        .then(response => {
-          alert('Success!', response)
-          submitButton.disabled = false
+          alert('Success!', response);
+          submitButton.disabled = false;
+          form.reset();
          })
        .catch(error => {
-       alert('Error!', error.message)
-         submitButton.disabled = false
-
-       }
-       )
-   })
+       alert('Error!', error.message);
+         submitButton.disabled = false;
+       });
+   });
+   return false;
+}
